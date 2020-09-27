299a57b912fa830c0f2f1e13d2fedbcc817fe092
Amir Abushareb <yields@icloud.com>
2020-09-27 15:30:41 +0300

    _examples: improve quotes

--- _examples/quotes/main.go
+++ _examples/quotes/main.go
@@ -2,27 +2,55 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"os"
 	"sync/atomic"
 	"time"
 
 	"github.com/yields/ant"
 )
 
-type counter struct {
-	pages uint64
+type quote struct {
+	Text string   `css:".text"`
+	By   string   `css:".author"`
+	Tags []string `css:".tag"`
 }
 
-func (c *counter) Scrape(ctx context.Context, p *ant.Page) ([]string, error) {
-	atomic.AddUint64(&c.pages, 1)
+type scraper struct {
+	quotes uint64
+	pages  uint64
+	enc    *json.Encoder
+}
+
+func (s *scraper) Scrape(ctx context.Context, p *ant.Page) ([]string, error) {
+	var items struct {
+		Quotes []quote `css:".quote"`
+	}
+
+	if err := p.Scan(&items); err != nil {
+		return nil, err
+	}
+
+	for _, q := range items.Quotes {
+		if err := s.enc.Encode(q); err != nil {
+			return nil, err
+		}
+	}
+
+	atomic.AddUint64(&s.quotes, uint64(len(items.Quotes)))
+	atomic.AddUint64(&s.pages, 1)
 	return p.URLs(), nil
 }
 
 func main() {
 	var url = "http://quotes.toscrape.com"
 	var ctx = context.Background()
-	var scraper = &counter{}
 	var start = time.Now()
+	var scraper = &scraper{
+		enc:   json.NewEncoder(os.Stdout),
+		pages: 0,
+	}
 
 	eng, err := ant.NewEngine(ant.EngineConfig{
 		Scraper: scraper,
@@ -36,8 +64,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Printf("scraped %d pages in %s :)",
+	log.Printf("scraped %d pages, %d quotes in %s :)",
 		scraper.pages,
+		scraper.quotes,
 		time.Since(start),
 	)
 }
